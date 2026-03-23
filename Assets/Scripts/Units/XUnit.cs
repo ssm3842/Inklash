7af5dd19746dd560eda92ee572ed9aad3e39b8b4
ssm3842 <ssm3842@naver.com>
2026-03-23 18:49:53 +0900

    오류 수정

---
+++ Assets/Scripts/Units/XUnit.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class XUnit : Units
+{
+    private bool isAccelerated = false;
+    private float updateTimer = 0f;
+
+    public override void OnDisruptEffect()
+    {
+        ResetBonus();
+        base.OnDisruptEffect();
+    }
+
+    public override void Move()
+    {
+        if (target || isAttacking)
+        {
+            updateTimer = 0f;
+            RB.linearVelocityX = 0f;
+            ANI.SetBool("IsMoving", false);
+            return;
+        }    
+
+        RB.linearVelocityX = isPlayers 
+        ? statController.GetStat(StatType.SPD) 
+        : -statController.GetStat(StatType.SPD);
+
+        ANI.SetBool("IsMoving", true);
+
+       updateTimer += Time.deltaTime;
+        if (updateTimer >= 1f && !isAccelerated)
+        {
+            StartAcceleration();
+        }
+    }
+
+    private void StartAcceleration()
+    {
+        statController.ControlBonusStat(StatType.SPD, 1);
+        statController.ControlBonusStat(StatType.ATK, 1);
+        isAccelerated = true;
+    }
+
+    protected override void PlayAttackAnimation()
+    {
+            ANI.SetTrigger("Attacked");
+    }
+
+    public override void _AttackEnemy()
+    {
+        base._AttackEnemy(); 
+        ResetBonus(); 
+    }
+
+    private void ResetBonus()
+    {
+        if(isAccelerated){
+        statController.ControlBonusStat(StatType.SPD, -1);
+        statController.ControlBonusStat(StatType.ATK, -1);
+
+        isAccelerated = false;
+        updateTimer = 0f;
+        }
+    }
+}
